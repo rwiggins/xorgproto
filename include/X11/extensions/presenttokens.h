rwiggins/xorgproto

commit 5a3d5d624e08cd8fcbe7f4da594dd458f45127d0

present: add PresentOptionAsyncMayTear

include/X11/extensions/presenttokens.h
@@ -45,11 +45,15 @@
 #define PresentOptionCopy               (1 << 1)
 #define PresentOptionUST                (1 << 2)
 #define PresentOptionSuboptimal         (1 << 3)
+#define PresentOptionAsyncMayTear       (1 << 4)
 
 #define PresentAllOptions       (PresentOptionAsync | \
                                  PresentOptionCopy | \
                                  PresentOptionUST | \
-                                 PresentOptionSuboptimal)
+                                 PresentOptionSuboptimal | \
+                                 PresentOptionAsyncMayTear)
+
+#define PresentAllAsyncOptions (PresentOptionAsync | PresentOptionAsyncMayTear)
 
 /* Present capabilities */
 
@@ -57,10 +61,14 @@
 #define PresentCapabilityAsync          1
 #define PresentCapabilityFence          2
 #define PresentCapabilityUST            4
+#define PresentCapabilityAsyncMayTear   8
 
 #define PresentAllCapabilities  (PresentCapabilityAsync | \
                                  PresentCapabilityFence | \
-                                 PresentCapabilityUST)
+                                 PresentCapabilityUST | \
+                                 PresentCapabilityAsyncMayTear)
+
+#define PresentAllAsyncCapabilities (PresentCapabilityAsync | PresentCapabilityAsyncMayTear)
 
 /* Events */
 #define PresentConfigureNotify	0

commit 85e11a6b73c711ed71ed606e76694ca42f46131e

presentproto: Update version number to 1.3
Bump the presentproto version to 1.3 in the pkg-config file,
presentproto.txt and presenttokens.h.

Signed-off-by: Olivier Fourdan <ofourdan@redhat.com>
Fixes: 5a3d5d624 - present: add PresentOptionAsyncMayTear

include/X11/extensions/presenttokens.h
@@ -25,7 +25,7 @@
 
 #define PRESENT_NAME			"Present"
 #define PRESENT_MAJOR			1
-#define PRESENT_MINOR			2
+#define PRESENT_MINOR			3
 
 #define PresentNumberErrors		0
 #define PresentNumberEvents		0

presentproto.pc.in
@@ -3,5 +3,5 @@ includedir=@includedir@
  
 Name: PresentProto
 Description: Present extension headers
-Version: 1.2
+Version: 1.3
 Cflags: -I${includedir}